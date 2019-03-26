954039f4caad223d1c084773dcdbbd05e84c2a96
Mike Trinh <mtrinhbla@gmail.com>
2019-03-26 15:34:17 -0400

    Fix installed apps bug

--- hpc_tm/settings.py
+++ hpc_tm/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tm.apps.TmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
