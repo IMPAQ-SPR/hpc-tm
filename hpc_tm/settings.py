786fb0a50b58601da77b80b6553ed18b4f393a82
Mike Trinh <mtrinhbla@gmail.com>
2019-03-26 17:02:47 -0400

    Fix various bug

--- hpc_tm/settings.py
+++ hpc_tm/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-with open('secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
