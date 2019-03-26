204a148dc3a78a11a863e6aa163e8ff3320bdc24
Mike Trinh <mtrinhbla@gmail.com>
2019-03-26 16:03:26 -0400

    Update settings

--- hpc_tm/settings.py
+++ hpc_tm/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u8^0vrsc%sz=upf$2!rahka=vek0=by=09i@s+kijo1kk8hodj'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
